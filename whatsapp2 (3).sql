6ff3a50d10c6c1a73d5a289c9305edbbe6e6db57
u-13h <urihdezflo1013@gmail.com>
2025-11-27 06:08:07 -0600

    Add files via upload

---
+++ whatsapp2 (3).sql
@@ -0,0 +1,226 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 27-11-2025 a las 13:07:31
+-- Versión del servidor: 10.4.32-MariaDB
+-- Versión de PHP: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `whatsapp2`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `chats`
+--
+
+CREATE TABLE `chats` (
+  `id_chat` bigint(20) NOT NULL,
+  `usuario1` int(11) NOT NULL,
+  `usuario2` int(11) NOT NULL,
+  `estado` enum('pendiente','aceptado','rechazado') DEFAULT 'pendiente'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `grupos`
+--
+
+CREATE TABLE `grupos` (
+  `id_grupo` bigint(20) NOT NULL,
+  `nombre` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `grupo_miembros`
+--
+
+CREATE TABLE `grupo_miembros` (
+  `id_grupo` bigint(20) NOT NULL,
+  `id_usuario` int(11) NOT NULL,
+  `rol` enum('admin','miembro') DEFAULT 'miembro',
+  `estado` enum('pendiente','aceptado','rechazado') DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `mensajes`
+--
+
+CREATE TABLE `mensajes` (
+  `id_mensaje` bigint(20) NOT NULL,
+  `id_chat` bigint(20) NOT NULL,
+  `emisor` int(11) NOT NULL,
+  `receptor` int(11) NOT NULL,
+  `texto` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `messages_grupo`
+--
+
+CREATE TABLE `messages_grupo` (
+  `id_mensaje` bigint(20) NOT NULL,
+  `id_grupo` bigint(20) NOT NULL,
+  `emisor` int(11) NOT NULL,
+  `texto` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuarios`
+--
+
+CREATE TABLE `usuarios` (
+  `id_usuario` int(11) NOT NULL,
+  `usuario` varchar(20) DEFAULT NULL,
+  `contrasena` varchar(20) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `usuarios`
+--
+
+INSERT INTO `usuarios` (`id_usuario`, `usuario`, `contrasena`) VALUES
+(1, 'uri', '1234'),
+(2, 'uri2', '12345');
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `chats`
+--
+ALTER TABLE `chats`
+  ADD PRIMARY KEY (`id_chat`),
+  ADD KEY `fk_usuario1` (`usuario1`),
+  ADD KEY `fk_usuario2` (`usuario2`);
+
+--
+-- Indices de la tabla `grupos`
+--
+ALTER TABLE `grupos`
+  ADD PRIMARY KEY (`id_grupo`);
+
+--
+-- Indices de la tabla `grupo_miembros`
+--
+ALTER TABLE `grupo_miembros`
+  ADD PRIMARY KEY (`id_grupo`,`id_usuario`),
+  ADD KEY `fk_gm_usuario` (`id_usuario`);
+
+--
+-- Indices de la tabla `mensajes`
+--
+ALTER TABLE `mensajes`
+  ADD PRIMARY KEY (`id_mensaje`),
+  ADD KEY `fk_msg_chat` (`id_chat`),
+  ADD KEY `fk_msg_emisor` (`emisor`),
+  ADD KEY `fk_msg_receptor` (`receptor`);
+
+--
+-- Indices de la tabla `messages_grupo`
+--
+ALTER TABLE `messages_grupo`
+  ADD PRIMARY KEY (`id_mensaje`),
+  ADD KEY `fk_mg_grupo` (`id_grupo`),
+  ADD KEY `fk_mg_emisor` (`emisor`);
+
+--
+-- Indices de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  ADD PRIMARY KEY (`id_usuario`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `chats`
+--
+ALTER TABLE `chats`
+  MODIFY `id_chat` bigint(20) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `grupos`
+--
+ALTER TABLE `grupos`
+  MODIFY `id_grupo` bigint(20) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `mensajes`
+--
+ALTER TABLE `mensajes`
+  MODIFY `id_mensaje` bigint(20) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `messages_grupo`
+--
+ALTER TABLE `messages_grupo`
+  MODIFY `id_mensaje` bigint(20) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  MODIFY `id_usuario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `chats`
+--
+ALTER TABLE `chats`
+  ADD CONSTRAINT `fk_usuario1` FOREIGN KEY (`usuario1`) REFERENCES `usuarios` (`id_usuario`),
+  ADD CONSTRAINT `fk_usuario2` FOREIGN KEY (`usuario2`) REFERENCES `usuarios` (`id_usuario`);
+
+--
+-- Filtros para la tabla `grupo_miembros`
+--
+ALTER TABLE `grupo_miembros`
+  ADD CONSTRAINT `fk_gm_grupo` FOREIGN KEY (`id_grupo`) REFERENCES `grupos` (`id_grupo`) ON DELETE CASCADE,
+  ADD CONSTRAINT `fk_gm_usuario` FOREIGN KEY (`id_usuario`) REFERENCES `usuarios` (`id_usuario`) ON DELETE CASCADE;
+
+--
+-- Filtros para la tabla `mensajes`
+--
+ALTER TABLE `mensajes`
+  ADD CONSTRAINT `fk_msg_chat` FOREIGN KEY (`id_chat`) REFERENCES `chats` (`id_chat`) ON DELETE CASCADE,
+  ADD CONSTRAINT `fk_msg_emisor` FOREIGN KEY (`emisor`) REFERENCES `usuarios` (`id_usuario`) ON DELETE CASCADE,
+  ADD CONSTRAINT `fk_msg_receptor` FOREIGN KEY (`receptor`) REFERENCES `usuarios` (`id_usuario`) ON DELETE CASCADE;
+
+--
+-- Filtros para la tabla `messages_grupo`
+--
+ALTER TABLE `messages_grupo`
+  ADD CONSTRAINT `fk_mg_emisor` FOREIGN KEY (`emisor`) REFERENCES `usuarios` (`id_usuario`) ON DELETE CASCADE,
+  ADD CONSTRAINT `fk_mg_grupo` FOREIGN KEY (`id_grupo`) REFERENCES `grupos` (`id_grupo`) ON DELETE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
