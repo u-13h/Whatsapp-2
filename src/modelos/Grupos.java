275b0f81e84b6e6fdc5b9480061c1373cebbe52e
SebastianSSFF <a21100314@ceti.mx>
2025-11-30 15:44:59 -0600

    Grupos

---
+++ src/modelos/Grupos.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package modelos;
+
+/**
+ *
+ * @author house
+ */
+public class Grupos {
+    private int idGrupo;
+    private String nombre;
+    
+    public Grupos(int idGrupo, String nombre)
+    {
+        this.idGrupo = idGrupo;
+        this.nombre = nombre;
+    }
+    
+    public Grupos(String nombre)
+    {
+        this.nombre = nombre;
+    }
+    
+    public int getIdGrupo()
+    {return idGrupo;}
+    public void setIdGrupo(int idGrupo)
+    {this.idGrupo = idGrupo;}
+    public String getNombre(){return nombre;}
+    public void setNombre(String nombre){this.nombre = nombre;}
+}
+
+
